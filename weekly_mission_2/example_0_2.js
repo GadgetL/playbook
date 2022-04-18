66f1f4239072714ff7c37302b808ec41a327cd30
GadgetL <laura.ramirezcosmes@gmail.com>
2022-04-18 10:37:30 -0500

    Example 0: object with parameters

---
+++ weekly_mission_2/example_0_2.js
@@ -0,0 +1,8 @@
+console.log("Objetos")
+    // Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Carlo",
+    age: 27
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc2)
