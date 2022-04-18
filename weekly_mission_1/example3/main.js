a5f2a2471178a417e2cff1598069437767cbafd4
GadgetL <laura.ramirezcosmes@gmail.com>
2022-04-17 20:46:23 -0500

    example4 for weekly mission 1, and rename files 'logger' in example3Â

--- weekly_mission_1/example3/main.js
+++ weekly_mission_1/example3/main.js
@@ -2,8 +2,8 @@
   node main.js
 */
 
-const logger1 = require('./loger_1')
-const logger2 = require('./loger_2')
+const logger1 = require('./logger_1')
+const logger2 = require('./logger_2')
 
 logger1('This is an informational message')
 logger2.verbose('This is a verbose message')
