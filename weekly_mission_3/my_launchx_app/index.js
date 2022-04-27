f9f70e3651e5923b807a595ba2b4d9e0b7a779a8
GadgetL <laura.ramirezcosmes@gmail.com>
2022-04-27 14:56:58 -0500

    build app 'my launch app', create class 'MissionCommander and add TDD

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+const lau = new MissionCommander("Lau")
+console.log(lau.name)
+const andy = new MissionCommander("Carlo")
+console.log(andy.name)
